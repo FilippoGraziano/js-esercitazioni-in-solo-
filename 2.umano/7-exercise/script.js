ec4b13e312477c2f680de0a353daf5907850adf4
Filippo Graziano <filippograziano13@gmail.com>
2026-05-22 15:47:41 +0200

    complete umano-7

---
+++ 2.umano/7-exercise/script.js
@@ -0,0 +1,7 @@
+// Determina se un array è vuoto.
+
+const names = [`Anna`, `Carlo`, `Filippo`, `Gianluca`, `Anastasia`, `Filiberto`, `Andrea`, `Franco`];
+
+const isArrEmpty = arr => console.log(arr.length === 0 ? true : false);
+
+isArrEmpty(names)
