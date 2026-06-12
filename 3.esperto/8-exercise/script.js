03ed06f46d519e472e3efc7627198a00405a66c4
Filippo Graziano <filippograziano13@gmail.com>
2026-06-12 15:16:11 +0200

    add new test and fixed esperto-8

--- 3.esperto/8-exercise/script.js
+++ 3.esperto/8-exercise/script.js
@@ -1,6 +1,8 @@
 // Ordina un array di numeri.
 
 const nums = [10, 2, 3, 5, 6, 20, 9, 8, 4, 7, 1, 21, 15, 13, 30, 40, 35];
+const nums2 = [10, 2, 8, 1, 5]
+
 
 const orderNumOfArr = arr => {
 
@@ -29,21 +31,25 @@ const orderNumOfArr = arr => {
                 console.log(`quindi aggiungo all'inizio: iterazione di i(${arr[i]}), (${orderedArr})`)
                 break;
 
-            } else if (arr[i] > orderedArr[j] && arr[i] < orderedArr[j + 1]) {
+            }
+
+            if (arr[i] > orderedArr[j] && arr[i] < orderedArr[j + 1]) {
 
                 console.log(`${arr[i]} è maggiore di ${orderedArr[j]}, ma è anche minore di ${orderedArr[j + 1]}`)
                 orderedArr.splice((j + 1), 0, arr[i])
                 console.log(`quindi aggiungo in mezzo: iterazione di i(${arr[i]}), (${orderedArr})`)
                 break;
 
-            } else if (arr[i] > orderedArr.at(-1)) {
+            }
+            
+            if (arr[i] > orderedArr.at(-1)) {
 
                 console.log(`${arr[i]} è maggiore di ${orderedArr.at(-1)}`)
                 orderedArr.push(arr[i]);
                 console.log(`quindi aggiungo alla fine: iterazione di i(${arr[i]}), (${orderedArr}))`)
                 break;
 
-            } else continue;
+            }
 
 
         }
@@ -52,5 +58,5 @@ const orderNumOfArr = arr => {
     return orderedArr;
 };
 
-console.log(nums)
-console.log(orderNumOfArr(nums));
+console.log(nums2)
+console.log(orderNumOfArr(nums2));
