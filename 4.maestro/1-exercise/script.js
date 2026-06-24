b9a2632bc1161a85c67e485c9a064a42c465f8cb
Filippo Graziano <filippograziano13@gmail.com>
2026-06-24 19:17:44 +0200

    fixed maestro-8

--- 4.maestro/1-exercise/script.js
+++ 4.maestro/1-exercise/script.js
@@ -1,11 +1,11 @@
 //r Determina se due stringhe sono anagrammi.
 
-const str1 = `Ciao, io sono Lollo_!-.2`;
+const str1 = `Ciao, io sono Lollò_!-.2`;
 const str2 = `Ciao, Lollo io sono@?:.3`;
 
 const splitWordOfString = string => {
 
-    const cleanString = string.toLowerCase().replaceAll(/\W|\d|[_]/g, ``);
+    const cleanString = string.toLowerCase().normalize(`NFD`).replace(/\W|\d|[_]/g, ``);
 
     const wordOfString = [];
     for (let i = 0; i < cleanString.length; i++) {
@@ -23,13 +23,13 @@ const wordCounter = string => {
     const words = [];
     for (let i = 0; i < cleanString.length; i++) {
 
-        if (countWords[`${cleanString[i]}`] === undefined) {
+        if (countWords[cleanString[i]] === undefined) {
 
             words.push(cleanString[i])
-            countWords[`${cleanString[i]}`] = 1
+            countWords[cleanString[i]] = 1
         } else {
 
-            countWords[`${cleanString[i]}`]++
+            countWords[cleanString[i]]++
         };
     };
 
@@ -41,10 +41,14 @@ const areAnagrams = (str1, str2) => {
     const [words1, countWords1] = wordCounter(str1);
     const [words2, countWords2] = wordCounter(str2);
 
+    const totalWordsOfStr1 = splitWordOfString(str1).length;
+    const totalWordsOfStr2 = splitWordOfString(str2).length;
+
+    if (totalWordsOfStr1 !== totalWordsOfStr2) return `No "${str1}" and "${str2}" are not anagrams :(`;
+
     for (let i = 0; i < words1.length; i++) {
 
-        if (countWords1[`${words1[i]}`] === countWords2[`${words1[i]}`]) continue;
-        else return `No "${str1}" and "${str2}" are not anagrams :(`; 
+        if (countWords1[words1[i]] !== countWords2[words1[i]]) return `No "${str1}" and "${str2}" are not anagrams :(`; 
     };
 
     return `Yes "${str1}" and "${str2}" are anagrams! :)`;
